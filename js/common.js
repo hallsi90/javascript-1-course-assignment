65e99a26d32717ee4be9280099b25a6abac97b12
Ingelinn Hallseth <ingelinn@hotmail.com>
2025-05-31 23:47:03 +0200

    Added scroll to top button

--- js/common.js
+++ js/common.js
@@ -120,3 +120,21 @@ function hideSpinner() {
     spinner.style.display = "none";
   }
 }
+
+/* -------------------------------
+   SCROLL TO TOP BUTTON
+-------------------------------- */
+
+const scrollBtn = document.getElementById("scrollToTopBtn");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    scrollBtn.classList.add("show");
+  } else {
+    scrollBtn.classList.remove("show");
+  }
+});
+
+scrollBtn.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
